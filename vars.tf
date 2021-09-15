cccce18772b9a14497d3a52a6dec5e5b486f86d3
Nabeel Raja <nabeel.raja@outlook.com>
2021-09-15 10:17:30 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,23 +1,19 @@
 variable "AWS_REGION" {
-  default = "eu-west-2"
+  default = "us-east-1"
 }
 
 variable "PRIVATE_KEY_PATH" {
-  default = "london-region-key-pair"
+  default = "rsa"
 }
 
 variable "PUBLIC_KEY_PATH" {
-  default = "london-region-key-pair.pub"
+  default = "rsa.pub"
 }
 
 variable "EC2_USER" {
   default = "ubuntu"
 }
-variable "AMI" {
-  type = "map"
 
-  default {
-    eu-west-2 = "ami-03dea29b0216a1e03"
-    us-east-1 = "ami-0c2a1acae6667e438"
-  }
+variable "AMI" {
+  default = "ami-0c2a1acae6667e438"
 }
